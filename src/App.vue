c574aec5bfbdf168e55c16b4a0926914489e806b
심지원 <shark.cametotea@gmail.com>
2019-03-24 23:53:38 +0900

    add main page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,58 @@
 <template>
-  
+  <v-card flat>
+    
+    <v-toolbar
+      color="#FB0B5C"
+      dark
+      extended
+      flat
+      height="100px"
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="display-2" >DOOTCHA</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon medium>account_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-layout row pb-2>
+      <v-flex xs8 offset-xs2>
+        <v-card class="card--flex-toolbar">
+          <v-toolbar card prominent>
+            <v-toolbar-title class="body-2 grey--text">Title</v-toolbar-title>
+            <v-layout >
+              <v-text-field
+                label="Solo"
+                placeholder="Placeholder"
+                solo
+                background-color="rgba(0,0,0,0)"
+                flat="false"
+              ></v-text-field>
+            </v-layout>
+            
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>apps</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-divider></v-divider>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
@@ -9,5 +62,7 @@ export default {
 </script>
 
 <style>
-
+  .card--flex-toolbar {
+    margin-top: -64px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import Vuex from 'vuex'
 import App from './App.vue'
-import router from './router.js'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-Vue.use(Vuex)
 
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
